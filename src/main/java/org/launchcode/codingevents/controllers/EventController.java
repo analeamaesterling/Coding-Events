51ba83e55ad680b34f6d1f867397cedc5e21786f
analeamaesterling <analeamaesterling@gmail.com>
2022-05-22 21:36:29 -0500

    Fixed events/events/create issue

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,31 +1,40 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("events") //where stuff lives
+@RequestMapping("events")
 public class EventController {
+
+    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Frustration hour");
-        events.add("code and cry");
-        events.add("Asking TAs for help");
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
-    //lives at /events
+
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String renderCreateEventForm(Model model){
+        model.addAttribute("events", events);
         return "events/create";
     }
 
+    //lives at /events/create (Handles a different kind of request than rendercreateeventform
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(new Event(eventName));
+        return "redirect:"; //will redirect to root path if you don't specify where
+    }
 }
