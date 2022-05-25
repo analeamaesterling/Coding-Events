9af189ca49eb2587ac5f161c85aa2ee23408947d
analeamaesterling <analeamaesterling@gmail.com>
2022-05-24 20:11:00 -0500

    added features

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,11 +1,39 @@
 package org.launchcode.codingevents.models;
 
+import org.springframework.boot.convert.DataSizeUnit;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+
 public class Event {
 
+    private int id;
+    private static int nextId = 1;
+
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
 
-    public Event(String name) {
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
+    private EventType type;
+
+    public Event(String name, String contactEmail, EventType type) {
+        this();
         this.name = name;
+        this.contactEmail = contactEmail;
+        this.type = type;
+
+    }
+
+    public Event() {
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -16,8 +44,37 @@ public class Event {
         this.name = name;
     }
 
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public EventType getType() { return type; }
+
+    public void setType(EventType type) { this.type = type; }
+
     @Override
     public String toString() {
         return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
